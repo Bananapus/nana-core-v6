25f68b00fee17e66f6dae506f1e7973584878db2
Jango <me.jango@protonmail.com>
2026-03-31 16:53:09 -0300

    style: align non-test files with guide

--- src/libraries/JBFixedPointNumber.sol
+++ src/libraries/JBFixedPointNumber.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.17;
+pragma solidity 0.8.28;
 
 library JBFixedPointNumber {
     function adjustDecimals(uint256 value, uint256 decimals, uint256 targetDecimals) internal pure returns (uint256) {
--- src/libraries/JBSplitGroupIds.sol
+++ src/libraries/JBSplitGroupIds.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity 0.8.28;
 
 /// @notice Group IDs that categorize splits.
 library JBSplitGroupIds {
